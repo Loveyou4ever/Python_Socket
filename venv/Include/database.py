2cd6579dff852ecc748acf88f34699606de7eb49
DESKTOP-EG7DSI8\75692 <756928869@qq.com>
2018-07-30 01:33:29 +0800

    2018-07-30 01:33:00

--- venv/Include/database.py
+++ venv/Include/database.py
@@ -1,36 +1,110 @@
 import json
-import urllib
 import pymysql
+import urllib
+from urllib import parse,request
 
-conn=pymysql.connect(host='127.0.0.1',user='root',passwd='123456',db='weather',port=3306,charset='utf8')
-#获取一个游标
-cursor = conn.cursor()
-sql = "select * from controller"
-cursor.execute(sql)
-print("cursor.excute:",cursor.rowcount)
+header_dict = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko',"Content-Type": "application/json"}
+url_temperatureIn= 'http://localhost:9091/InsertTempIn'
+url_temperatureOut= 'http://localhost:9091/InsertTempOut'
+url_humidityIn= 'http://localhost:9091/InsertHumiIn'
+url_humidityOut= 'http://localhost:9091/InsertHumiOut'
+url_illumination_intensity_in= 'http://localhost:9091/IlluminationInInesrt'
+url_illumination_intensity_out= 'http://localhost:9091/IlluminationOutInesrt'
+url_ultraviolet_ray= 'http://localhost:9091/UltravioletInsert'
+url_air_speed= 'http://localhost:9091/AirSpeedInsert'
+url_wind_direction= 'http://localhost:9091/WindDirectionInsert'
+url_atmospheric_pressure= 'http://localhost:9091/AtmoPreInsert'
+
+#Database Operation
+#conn=pymysql.connect(host='127.0.0.1',user='root',passwd='123456',db='weather',port=3306,charset='utf8')
+# cursor = conn.cursor()
+# sql = "select * from temperature_indoor"
+# cursor.execute(sql)
+
+example_str = "atmos=10111"
+
+key_str = example_str[0:5]
+value_str = example_str[6:]
+print("key:", key_str)
+print("value:", value_str)
 #封装json数据格式
 #json.dumps({'key':value})
-temperatureIn = json.dumps({'temperature': 27.2}, sort_keys=True, indent=4, separators=(',', ': '))
-temperatureOut = json.dumps({'temperature': 38.9}, sort_keys=True, indent=4, separators=(',', ': '))
-humidityIn = json.dumps({'humidity': 40.1}, sort_keys=True, indent=4, separators=(',', ': '))
-humidityOut = json.dumps({'humidity': 35.7}, sort_keys=True, indent=4, separators=(',', ': '))
-illumination_intensity_in = json.dumps({'illumination': 200}, sort_keys=True, indent=4, separators=(',', ': '))
-illumination_intensity_out = json.dumps({'illumination': 10000}, sort_keys=True, indent=4, separators=(',', ': '))
-ultraviolet_ray = json.dumps({'ultraviolet': 10}, sort_keys=True, indent=4, separators=(',', ': '))
-air_speed = json.dumps({'airSpeed': 15.0}, sort_keys=True, indent=4, separators=(',', ': '))
-wind_direction = json.dumps({'windDirection': "South-East"}, sort_keys=True, indent=4, separators=(',', ': '))
-atmospheric_pressure = json.dumps({'atmosphericPressure': 10090}, sort_keys=True, indent=4, separators=(',', ': '))
+if(key_str == "temp1"):
+    temperatureIn = json.dumps({'temperature': value_str}, sort_keys=True, indent=4, separators=(',', ': ')).encode(encoding='utf-8')
+    req1 = request.Request(url=url_temperatureIn, data=temperatureIn, headers=header_dict)
+    res1 = request.urlopen(req1)
+    res1 = res1.read()
+    print(res1)
+if(key_str == "temp2"):
+    temperatureOut = json.dumps({'temperature': value_str}, sort_keys=True, indent=4, separators=(',', ': ')).encode(encoding='utf-8')
+    req2 = request.Request(url=url_temperatureOut, data=temperatureOut, headers=header_dict)
+    res2 = request.urlopen(req2)
+    res2 = res2.read()
+    print(res2)
+if(key_str == "humi1"):
+    humidityIn = json.dumps({'humidity': value_str}, sort_keys=True, indent=4, separators=(',', ': ')).encode(encoding='utf-8')
+    req3 = request.Request(url=url_humidityIn, data=humidityIn, headers=header_dict)
+    res3 = request.urlopen(req3)
+    res3 = res3.read()
+    print(res3)
+if(key_str == "humi2"):
+    humidityOut = json.dumps({'humidity': value_str}, sort_keys=True, indent=4, separators=(',', ': ')).encode(encoding='utf-8')
+    req4 = request.Request(url=url_humidityOut, data=humidityOut, headers=header_dict)
+    res4 = request.urlopen(req4)
+    res4 = res4.read()
+    print(res4)
+if (key_str == "ligh1"):
+    illumination_intensity_in = json.dumps({'illumination': value_str}, sort_keys=True, indent=4,separators=(',', ': ')).encode(encoding='utf-8')
+    req5 = request.Request(url=url_illumination_intensity_in, data=illumination_intensity_in, headers=header_dict)
+    res5 = request.urlopen(req5)
+    res5 = res5.read()
+    print(res5)
+if (key_str == "ligh2"):
+    illumination_intensity_out = json.dumps({'illumination': value_str}, sort_keys=True, indent=4,separators=(',', ': ')).encode(encoding='utf-8')
+    req6 = request.Request(url=url_illumination_intensity_out, data=illumination_intensity_out, headers=header_dict)
+    res6 = request.urlopen(req6)
+    res6 = res6.read()
+    print(res6)
+if (key_str == "ultra"):
+    ultraviolet_ray = json.dumps({'ultraviolet': value_str}, sort_keys=True, indent=4, separators=(',', ': ')).encode(encoding='utf-8')
+    req7 = request.Request(url=url_ultraviolet_ray, data=ultraviolet_ray, headers=header_dict)
+    res7 = request.urlopen(req7)
+    res7 = res7.read()
+    print(res7)
+if (key_str == "speed"):
+    air_speed = json.dumps({'airSpeed': value_str}, sort_keys=True, indent=4, separators=(',', ': ')).encode(encoding='utf-8')
+    req8 = request.Request(url=url_air_speed, data=air_speed, headers=header_dict)
+    res8 = request.urlopen(req8)
+    res8 = res8.read()
+    print(res8)
+if (key_str == "direc"):
+    wind_direction = json.dumps({'windDirection': value_str}, sort_keys=True, indent=4,separators=(',', ': ')).encode(encoding='utf-8')
+    req9 = request.Request(url=url_wind_direction, data=wind_direction, headers=header_dict)
+    res9 = request.urlopen(req9)
+    res9 = res9.read()
+    print(res9)
+if(key_str == "atmos"):
+    atmospheric_pressure = json.dumps({'atmosphericPressure': value_str}, sort_keys=True, indent=4,separators=(',', ': ')).encode(encoding='utf-8')
+    req10 = request.Request(url=url_atmospheric_pressure, data=atmospheric_pressure, headers=header_dict)
+    res10 = request.urlopen(req10)
+    res10 = res10.read()
+    print(res10)
 
-print (temperatureIn)
-print (temperatureOut)
-print (humidityIn)
-print (humidityOut)
-print (illumination_intensity_in)
-print (illumination_intensity_out)
-print (ultraviolet_ray)
-print (air_speed)
-print (wind_direction)
-print (atmospheric_pressure)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- venv/Include/raspberry Pi3.py
+++ venv/Include/raspberry Pi3.py
@@ -5,33 +5,13 @@ while(True):
     url = "http://localhost:9091/Raspberry"
     stdout = urllib.request.urlopen(url)
     ControlResource = stdout.read().decode('utf-8')
-
     jsonData = json.loads(ControlResource)
     key = list(jsonData["data"].keys())
     value = list(jsonData["data"].values())
     print("keys: ", key)
-    print("values: ", value)
-    for x in value:
-        if x == '0':
-            print("人工控制")
-        if x == '1':
-            print("自动控制")
-        if x == '1001':
-            print("Air Conditioner On!")
-        if x == '1002':
-            print("Air Conditioner Off!")
-        if x == '1003':
-            print("Humidifier On")
-        if x == '1004':
-            print("Humidifier Off")
-        if x == '1005':
-            print("Light On")
-        if x == '1006':
-            print("Light Off!")
-        if x == '1007':
-            print("Windows Off!")
-        if x == '1008':
-            print("Windows Off!")
+    if key == ['control_mode']:
+        data = value[0];
+        print("data: ", data)
     time.sleep(5)
 
 
